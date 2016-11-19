3a35ffc5562f038f4da846ba79f64751d6642c68
Andrej Matijevic <andrej.matijevic@gmail.com>
2016-11-19 18:00:48 +0100

    Override startup host configuration for Server listening url's and environment using environment variables

---
+++ CustomHostSetup/src/ServerUrlsAndEnvironment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace ServerUrlsAndEnvironment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Just uncomment combinations that you want to try
+            //Order mater - last added overrides previous
+            var configuration = new ConfigurationBuilder()
+                //.AddEnvironmentVariables()
+                //.AddEnvironmentVariables("MYVALUES_")
+                .Build();
+
+            //var environment = configuration["ASPNETCORE_ENVIRONMENT"];
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                //.UseConfiguration(configuration)
+                //.UseEnvironment(environment)
+                .Configure(appBuilder => appBuilder.Run(async context =>
+                    {
+                        IHostingEnvironment env = (IHostingEnvironment)appBuilder.ApplicationServices.GetService(typeof(IHostingEnvironment));
+                        var serverUrls = appBuilder.ServerFeatures.Get<IServerAddressesFeature>()?.Addresses;
+                        await context.Response.WriteAsync($"Environment (ASPNETCORE_ENVIRONMENT): {env.EnvironmentName}\n");
+                        await context.Response.WriteAsync($"Listening on address (SERVER.URLS):{string.Join(",", serverUrls)}\n");
+
+                    }))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
